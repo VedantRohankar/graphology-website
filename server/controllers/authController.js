82a1635295535521490672613988b54eb23e5e82
vedant <vedantrohankar1@gmail.com>
2026-06-20 15:22:17 +0530

    Login API created

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import { createUser,findUserEmail } from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req,res)=>{
   try {
@@ -30,5 +31,50 @@ export const register = async (req,res)=>{
       message:"Server Error",
     });
     
+  }
+};
+
+export const login = async(req,res)=>{
+
+  try {
+    const {email,password} = req.body;
+
+    const user = await findUserEmail(email);
+
+    if(!user){
+      return res.status(400).json({
+        message:"Invalid Email or Password",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password,user.password);
+
+    if(!isMatch){
+      return res.status(400).json({
+        message:"Invalid Email or Password",
+      });
+    }
+
+    const token = jwt.sign(
+      {id:user.id},
+      process.env.JWT_SECRET,
+      {expiresIn:"1d"}
+    );
+
+    res.json({
+      message:"Login Successful",
+      token,
+      user:{
+        id:user.id,
+        username:user.username,
+        email:user.email,
+      },
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message:"Server Error",
+    });
   }
 };
